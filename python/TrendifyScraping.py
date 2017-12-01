dd4213b1d8538824cead7d1e28bbfcbba4f4133e
JamesPHawk <pjhawk208@gmail.com>
2017-12-01 11:22:46 -0500

    Started authenticating app

--- python/TrendifyScraping.py
+++ python/TrendifyScraping.py
@@ -1,2 +1,8 @@
-print("Hello")
-print("Adding to this file")
+import spotipy
+import spotipy.util as util
+scope = 'user-library-read'
+token = util.prompt_for_user_token("UnduePeej", scope, client_id="7a90865bc671479797914d96ba7d3021", client_secret="0abdd042f90e41adb88dd064c0c3f444", redirect_uri="trendify://callback/")
+spy = spotipy.Spotify(auth=token)
+name = "Kendrick Lamar"
+results = spy.search(q='artist:' + name, type='artist')
+print(results)
